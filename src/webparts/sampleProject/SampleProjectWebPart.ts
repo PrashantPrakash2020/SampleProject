f22bf6eec00f6a19b8a841f2bedaeffba64d4713
PRASHANT PRAKASH <capgemini.prakash@daimler.com>
2022-01-23 15:09:04 +0530

    my commit

---
+++ src/webparts/sampleProject/SampleProjectWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'SampleProjectWebPartStrings';
+import SampleProject from './components/SampleProject';
+import { ISampleProjectProps } from './components/ISampleProjectProps';
+
+export interface ISampleProjectWebPartProps {
+  description: string;
+}
+
+export default class SampleProjectWebPart extends BaseClientSideWebPart<ISampleProjectWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<ISampleProjectProps > = React.createElement(
+      SampleProject,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/sampleProject/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISampleProjectWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SampleProjectWebPartStrings' {
+  const strings: ISampleProjectWebPartStrings;
+  export = strings;
+}
